9f81be31f76ce63ddadec682c2e0df7279d15290
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-05-11 18:58:26 +0200

    The basic idea

---
+++ CodeEditor.gd
@@ -0,0 +1,49 @@
+extends PanelContainer
+
+export (GDScript) var initial_text_source: GDScript = null
+
+onready var editor: TextEdit = find_node	("TextEdit")
+onready var run_button: Button = find_node("RunButton")
+
+func _ready():
+	_create_run_shortcut()
+	run_button.connect("pressed", self, "_on_run_button_pressed")
+	
+	_load_initial_script()
+	
+func _input(event: InputEvent):
+	if (event is InputEventKey
+	&& event.pressed
+	&& event.alt):
+		match event.scancode:
+			KEY_LEFT: editor.cursor_set_line(
+				editor.cursor_get_line() - 1)
+			KEY_RIGHT: editor.cursor_set_line(
+				editor.cursor_get_line() + 1)
+	
+func _on_run_button_pressed():
+	var target: Camera = get_viewport().get_camera()
+	var script := GDScript.new()
+	script.source_code = PoolStringArray([
+		# todo: insert setup code
+		editor.text,
+	]).join("\n")
+	script.reload(true)
+	target.set_script(script)
+	target.request_ready()
+	
+func _create_run_shortcut():
+	var sc := ShortCut.new()
+	var ev := InputEventKey.new()
+	ev.scancode = KEY_R
+	ev.control = true
+	sc.shortcut = ev
+	run_button.shortcut = sc
+	run_button.shortcut_in_tooltip = true
+	
+func _load_initial_script():
+	if not initial_text_source:
+		return
+	editor.text = initial_text_source.source_code
+	
+# end
---
+++ LiveEnv.gd
@@ -0,0 +1,17 @@
+extends Node
+
+class_name LiveEnv
+
+func _ready():
+	prints("Setting up LiveEnv...")
+	
+	# using external files?
+		# find project files
+		# start file watchers
+	# using internal files?
+		# load project blob
+		# create internal-file instances
+	# load ui
+	# load 2d or 3d stuff
+	
+	prints("Done!")
